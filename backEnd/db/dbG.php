26450f612abf020e771d1081cdfd9c1e5e7e1d3a
Davide <musitelli.davideluigi.studente@itispaleocapa.it>
2022-01-25 20:22:16 +0100

    update

--- backEnd/db/dbG.php
+++ backEnd/db/dbG.php
@@ -1,22 +1,15 @@
 <?php
-   $dbhost = 'ec2-52-208-221-89.eu-west-1.compute.amazonaws.com';
-   $dbuser = 'colbpsouojhqbl';
-   $dbpass = '0246d8a225c675bd471cdde69e9f28f4992a190626d58dbcaa549a30e09197aa';
-   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-   
-   if(! $conn ) {
-      die('Could not connect: ' . mysql_error());
-   }
-   
-   echo 'Connected successfully';
-   
-   $sql = 'CREATE Database test_db';
-   $retval = mysql_query( $sql, $conn );
-   
-   if(! $retval ) {
-      die('Could not create database: ' . mysql_error());
-   }
-   
-   echo "Database test_db created successfully\n";
-   mysql_close($conn);
+$servername = "ec2-52-208-221-89.eu-west-1.compute.amazonaws.com";
+$username = "colbpsouojhqbl";
+$password = "0246d8a225c675bd471cdde69e9f28f4992a190626d58dbcaa549a30e09197aa";
+
+$conn = new mysqli($servername,
+			$username, $password);
+
+if ($conn->connect_error) {
+	die("Connection failure: "
+		. $conn->connect_error);
+}
+
+$conn->close();
 ?>
