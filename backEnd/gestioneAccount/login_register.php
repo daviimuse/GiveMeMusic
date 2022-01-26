ee634e64a81ce00bd331618b8b688f8ad64d05a1
Davide <musitelli.davideluigi.studente@itispaleocapa.it>
2022-01-26 10:40:23 +0000

    update

---
+++ backEnd/gestioneAccount/login_register.php
@@ -0,0 +1,37 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["Rpassword"])){//Registrazione
+    $name = $_POST["name"];
+    $mail = $_POST["mail"];
+    $psw = $_POST["Rpassword"];
+    //Controllare che utente non esista già
+    $car = "accounts/" .hash(md5, $mail)  . "/";
+    chmod("accounts/", 0755);
+    if(!file_exists($car)){
+        mkdir($car);
+        $file = fopen($car . "psw.txt", "w") or die("Unable to open file!");
+        fwrite($file, hash(md5, $psw));
+        fclose($file);
+        echo "Utente registrato";
+    }else{
+        echo "Utente già registrato";
+    }
+}elseif($_SERVER['REQUEST_METHOD'] == 'POST'){//Login
+    $mail = $_POST["mail"];
+    $psw = $_POST["password"];
+    $car = "accounts/" . hash(md5, $mail)  . "/";
+    if(file_exists($car)){
+        $file = fopen($car . "psw.txt", "r") or die("Unable to open file!");
+        $hashpsw=fread($file,filesize($car . "psw.txt"));
+        fclose($file);
+        if(hash(md5, $psw) == $hashpsw){
+            // echo "Password corretta";
+            // echo "Login effettuato";
+            upload();
+        }else{
+            echo "Password sbagliata";
+        }
+    }else{
+        echo "Utente non presente. Effettuare la registrazione.";
+    }
+}
+?>
