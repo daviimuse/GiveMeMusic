b78b9154258130dc9c6c302a484e56495eab559d
Davide <musitelli.davideluigi.studente@itispaleocapa.it>
2022-02-10 15:24:34 +0000

    Organizing files

--- myApp/index.php
+++ myApp/index.php
@@ -35,10 +35,13 @@
 									<div>
 										<button method="POST" name="gB"><?php 
 										include('./backEnd/OAuth2/config.php');
+
 										$login_button = '';
+
 										if(isset($_POST['gB'])){
 											$login_button = $_POST["gB"];
 										}
+
 										if(isset($_GET["code"]))
 										{
 											$token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
@@ -60,18 +63,20 @@
 											if(!empty($data['picture'])){$_SESSION['user_image'] = $data['picture'];}
 											}
 										}
+
 										if(!isset($_SESSION['access_token'])){
 											$login_button = '<a href="'.$google_client->createAuthUrl().'" style="color:white;">Login With Google</a>';
 										}
+
 										if($login_button == '')
 										{
-										echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
-										echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
-										echo '<h3><b>Name :</b> '.$_SESSION['user_first_name'].' '.$_SESSION['user_last_name'].'</h3>';
-										echo '<h3><b>Email :</b> '.$_SESSION['user_email_address'].'</h3>';
-										echo '<h3><a href="logout.php">Logout</h3></div>';
+											echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
+											echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
+											echo '<h3><b>Name :</b> '.$_SESSION['user_first_name'].' '.$_SESSION['user_last_name'].'</h3>';
+											echo '<h3><b>Email :</b> '.$_SESSION['user_email_address'].'</h3>';
+											echo '<h3><a href="logout.php">Logout</h3></div>';
 										}else{
-										echo '<div>'.$login_button . '</div>';
+											echo '<div>'.$login_button . '</div>';
 										}
 										?></button>
 									</div>
