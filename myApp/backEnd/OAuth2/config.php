b4700ae06430994f20930016c25313051dc94f24
Davide <musitelli.davideluigi.studente@itispaleocapa.it>
2022-02-15 08:00:53 +0000

    update

--- myApp/backEnd/OAuth2/config.php
+++ myApp/backEnd/OAuth2/config.php
@@ -1,18 +1,18 @@
 <?php
-session_start();
+    session_start();
 
-require_once 'vendor/autoload.php';//Include Google Client Library for PHP autoload file
+    require_once 'vendor/autoload.php';//Include Google Client Library for PHP autoload file
 
-$google_client = new Google_Client();
+    $google_client = new Google_Client();
 
-$google_client->setClientId('259230819724-stdqra0rpbqrsih6upusf32si86jvis5.apps.googleusercontent.com');//OAuth Client ID
+    $google_client->setClientId('259230819724-stdqra0rpbqrsih6upusf32si86jvis5.apps.googleusercontent.com');//OAuth Client ID
 
-$google_client->setClientSecret('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');//OAuth Client Secret key
+    $google_client->setClientSecret('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');//OAuth Client Secret key
 
-$google_client->setRedirectUri('http://localhost/Gmm/index.php');//OAuth Redirect URI
+    $google_client->setRedirectUri('http://localhost/Gmm/index.php');//OAuth Redirect URI
 
-$google_client->addScope('email');
+    $google_client->addScope('email');
 
-$google_client->addScope('profile');
+    $google_client->addScope('profile');
 
 ?>
