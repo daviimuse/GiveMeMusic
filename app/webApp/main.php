712281c4d683f3170f6648327ae682d6befaa31a
Davide <musitelli.davideluigi.studente@itispaleocapa.it>
2022-03-10 11:55:57 +0000

    Fixing Logout feature

--- app/webApp/main.php
+++ app/webApp/main.php
@@ -1,101 +1,103 @@
-<?php
-    require_once '.././backEnd/OAuth2/google/vendor/autoload.php';
-    session_start();
-    $clientID = '440831399970-k5vj91pqnpvev88t793k4oemqsapsbcm.apps.googleusercontent.com';
-    $clientSecret = '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';
-    $redirectUri = 'http://papopep.altervista.org/GiveMeMusic-main/webApp/main.php'; 
-    $client = new Google_Client();
-    $client->setClientId($clientID);
-    $client->setClientSecret($clientSecret);
-    $client->setRedirectUri($redirectUri);
-    $client->addScope("email");
-    $client->addScope("profile");
-    if(isset($_GET['code'])) {
-        $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-        $client->setAccessToken($token['access_token']);
-        $google_oauth = new Google_Service_Oauth2($client);
-        $google_account_info = $google_oauth->userinfo->get();
-        $user =  $google_account_info->name;
-        $mail =  $google_account_info->email;
-    }else{
-        $user =  $_SESSION['usr'];
-        $mail = $_SESSION['mail'];
-    }
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>Home Page</title>
-    <link rel="stylesheet" href="../frontEnd/assets/cssFolder/main.css">
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
-<body>
-  <div class="sidebar">
-    <div class="logo-details">
-        <i class='bx bx-menu' id="btn" ></i>
-    </div>
-    <ul class="nav-list">
-      <li>
-          <i class='bx bx-search' ></i>
-         <input type="text" placeholder="Search...">
-         <span class="tooltip">Search</span>
-      </li>
-      <li>
-        <a href="listen.php">
-          <i class='bx bx-grid-alt'></i>
-          <span class="links_name">Choose your music</span>
-        </a>
-         <span class="tooltip">Choose your music</span>
-      </li>
-     <li>
-       <a href="fsongs.php">
-         <i class='bx bx-heart' ></i>
-         <span class="links_name">Favorite songs</span>
-       </a>
-       <span class="tooltip">Favorite songs</span>
-     </li>
-     <!-- <li>
-         <a href="#">
-           <i class='bx bx-user' ></i>
-           <span class="links_name">User</span>
-         </a>
-         <span class="tooltip">User</span>
-     </li> -->
-     <li>
-       <a href="#">
-         <i class='bx bx-cog' ></i>
-         <span class="links_name">Setting</span>
-       </a>
-       <span class="tooltip">Setting</span>
-     </li>
-     <li class="profile">
-         <div class="profile-details">
-            <div class="name_job">
-        <?php  
-            if(isset($user)){
-                echo '<div class="name">'.$user.'</div>';
-                echo '<div class="job">'.$mail.'</div>';
-            }else{
-                echo '<div class="name">Piero Angela</div>';
-            }
-        ?>
-            </div>
-         </div>
-        <i class='bx bx-log-out' id="log_out"></i>
-     </li>
-    </ul>
-  </div>
-  <section class="home-section">
-    <?php
-        if(isset($user)){
-          echo '<div class="text">Welcome '.$user.' to Give Me Music!<br>'; 
-        }else{
-          echo '<div class="text">Welcome user to Give Me Music!<br>';
-        }
-    ?>
-</section>
-  <script src="../frontEnd/assets/jsFolder/script.js"></script>
-</body>
+<?php
+    require_once '.././backEnd/OAuth2/google/vendor/autoload.php';
+    session_start();
+    $clientID = '440831399970-k5vj91pqnpvev88t793k4oemqsapsbcm.apps.googleusercontent.com';
+    $clientSecret = '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';
+    $redirectUri = 'http://papopep.altervista.org/GiveMeMusic-main/webApp/main.php'; 
+    $client = new Google_Client();
+    $client->setClientId($clientID);
+    $client->setClientSecret($clientSecret);
+    $client->setRedirectUri($redirectUri);
+    $client->addScope("email");
+    $client->addScope("profile");
+    if(isset($_GET['code'])) {
+        $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+        $client->setAccessToken($token['access_token']);
+        $google_oauth = new Google_Service_Oauth2($client);
+        $google_account_info = $google_oauth->userinfo->get();
+        $user =  $google_account_info->name;
+        $mail =  $google_account_info->email;
+    }else{
+        $user =  $_SESSION['usr'];
+        $mail = $_SESSION['mail'];
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="../frontEnd/assets/cssFolder/main.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="sidebar">
+    <div class="logo-details">
+        <i class='bx bx-menu' id="btn" ></i>
+    </div>
+    <ul class="nav-list">
+      <li>
+          <i class='bx bx-search' ></i>
+         <input type="text" placeholder="Search...">
+         <span class="tooltip">Search</span>
+      </li>
+      <li>
+        <a href="listen.php">
+          <i class='bx bx-grid-alt'></i>
+          <span class="links_name">Choose your music</span>
+        </a>
+         <span class="tooltip">Choose your music</span>
+      </li>
+     <li>
+       <a href="fsongs.php">
+         <i class='bx bx-heart' ></i>
+         <span class="links_name">Favorite songs</span>
+       </a>
+       <span class="tooltip">Favorite songs</span>
+     </li>
+     <!-- <li>
+         <a href="#">
+           <i class='bx bx-user' ></i>
+           <span class="links_name">User</span>
+         </a>
+         <span class="tooltip">User</span>
+     </li> -->
+     <li>
+       <a href="#">
+         <i class='bx bx-cog' ></i>
+         <span class="links_name">Setting</span>
+       </a>
+       <span class="tooltip">Setting</span>
+     </li>
+     <li class="profile">
+         <div class="profile-details">
+            <div class="name_job">
+        <?php  
+            if(isset($user)){
+                echo '<div class="name">'.$user.'</div>';
+                echo '<div class="job">'.$mail.'</div>';
+            }else{
+                echo '<div class="name">Piero Angela</div>';
+            }
+        ?>
+            </div>
+         </div>
+         <form action="../logOut.php" method="POST">
+            <button class='bx bx-log-out' id="log_out" style="color: white;"></button>
+        </form>
+     </li>
+    </ul>
+  </div>
+  <section class="home-section">
+    <?php
+        if(isset($user)){
+          echo '<div class="text">Welcome '.$user.' to Give Me Music!<br>'; 
+        }else{
+          echo '<div class="text">Welcome user to Give Me Music!<br>';
+        }
+    ?>
+</section>
+  <script src="../frontEnd/assets/jsFolder/script.js"></script>
+</body>
 </html>
