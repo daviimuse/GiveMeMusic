c0417cf6cbc0f62453cb2560c583896ee1f537c1
Davide <84710848+daviimuse@users.noreply.github.com>
2022-05-27 11:10:48 +0200

    Update user.php

--- app/user.php
+++ app/user.php
@@ -1,350 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>User infos</title>
-    <link rel="stylesheet" type="text/css" href=".././frontEnd/assets/loginFolders/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href=".././frontEnd/assets/loginFolders/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href=".././frontEnd/assets/loginFolders/fonts/iconic/css/material-design-iconic-font.min.css">
-    <link rel="stylesheet" type="text/css" href=".././frontEnd/assets/loginFolders/vendor/animate/animate.css">
-    <link rel="stylesheet" type="text/css" href=".././frontEnd/assets/loginFolders/vendor/css-hamburgers/hamburgers.min.css">
-    <link rel="stylesheet" type="text/css" href=".././frontEnd/assets/loginFolders/vendor/animsition/css/animsition.min.css">
-    <link rel="stylesheet" type="text/css" href=".././frontEnd/assets/loginFolders/vendor/select2/select2.min.css">
-    <link rel="stylesheet" type="text/css" href=".././frontEnd/assets/loginFolders/vendor/daterangepicker/daterangepicker.css">
-    <link rel="stylesheet" type="text/css" href=".././frontEnd/assets/loginFolders/css/util.css">
-    <link rel="stylesheet" type="text/css" href=".././frontEnd/assets/loginFolders/css/main.css">
-    <link rel="stylesheet" href="frontEnd/assets/cssFolder/user.css">
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="backEnd/gestioneDB/request.js"></script>
-</head>
-<?php
-    session_start();
-
-    $id_user = $_SESSION['id'];
-
-    require_once 'backEnd/OAuth2/google/vendor/autoload.php';
-    $clientID = '259230819724-stdqra0rpbqrsih6upusf32si86jvis5.apps.googleusercontent.com';
-    $clientSecret = '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';
-    $redirectUri = 'https://localhost/GiveMeMusic/user.php'; 
-    $client = new Google_Client();
-    $client->setClientId($clientID);
-    $client->setClientSecret($clientSecret);
-    $client->setRedirectUri($redirectUri);
-    $client->addScope("email");
-    $client->addScope("profile");
-    if(isset($_GET['code'])) {
-        $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-        $client->setAccessToken($token['access_token']);
-        $google_oauth = new Google_Service_Oauth2($client);
-        $google_account_info = $google_oauth->userinfo->get();
-        $user =  $google_account_info->name;
-        $mail =  $google_account_info->email;
-    }
-
-    // var_dump($_SESSION);
-        if(isset($_SESSION["UID"])){
-?>
-<body>
-<div class="sidebar">
-      <div class="logo-details">
-          <i class='bx bx-menu' id="btn" ></i>
-      </div>
-      <ul class="nav-list">
-      <div id="searchBox">  
-
-      <li>
-          <a href="home.php">
-            <i class='bx bx-home'></i>
-            <span class="links_name">Home</span>
-          </a>
-          <span class="tooltip">Home</span>
-        </li>
-        
-        <li>
-            <a href="explore.php">
-              <i class='bx bx-grid-alt'></i>
-              <span class="links_name">Explore</span>
-            </a>
-            <span class="tooltip">Explore</span>
-          </li>
-          
-      <li>
-        <a href="preferences.php">
-          <i class='bx bx-heart' ></i>
-          <span class="links_name">Preferences</span>
-        </a>
-        <span class="tooltip">Preferences</span>
-      </li>
-
-      <li>
-          <a href="user.php">
-            <i class='bx bx-user' ></i>
-            <span class="links_name">User</span>
-          </a>
-          <span class="tooltip">User</span>
-      </li>
-
-      <!-- <li>
-        <a href="settings.php">
-          <i class='bx bx-cog' ></i>
-          <span class="links_name">Settings</span>
-        </a>
-        <span class="tooltip">Settings</span>
-      </li> -->
-
-      <li class="profile">
-          <div class="profile-details">
-              <div class="name_job" style="color:white;">GiveMeMusic &#8482;</div>
-          </div>
-              <form action="logOut.php" method="POST">
-                  <button class='bx bx-log-out' id="log_out" style="color: white;"></button>
-              </form>
-        </li>
-      </ul>
-    </div>
-    <section class="home-section">
-        <div class="text" style="height: 100%; width: 100%; display: flex; position: fixed; align-items: center; justify-content: center;">!User Logged, Work In Progress!
-        </div>
-    </section>
-<script src="frontEnd/assets/jsFolder/script.js"></script>
-</body>
-</html>
-<?php
-        }else{        
-?>
-<body>
-<div class="sidebar">
-      <div class="logo-details">
-          <i class='bx bx-menu' id="btn" ></i>
-      </div>
-      <ul class="nav-list">
-      <div id="searchBox">  
-
-      <li>
-          <a href="home.php">
-            <i class='bx bx-home'></i>
-            <span class="links_name">Home</span>
-          </a>
-          <span class="tooltip">Home</span>
-        </li>
-        
-        <li>
-            <a href="explore.php">
-              <i class='bx bx-grid-alt'></i>
-              <span class="links_name">Explore</span>
-            </a>
-            <span class="tooltip">Explore</span>
-          </li>
-          
-      <li>
-        <a href="preferences.php">
-          <i class='bx bx-heart' ></i>
-          <span class="links_name">Preferences</span>
-        </a>
-        <span class="tooltip">Preferences</span>
-      </li>
-
-      <li>
-          <a href="user.php">
-            <i class='bx bx-user' ></i>
-            <span class="links_name">User</span>
-          </a>
-          <span class="tooltip">User</span>
-      </li>
-
-      <!-- <li>
-        <a href="settings.php">
-          <i class='bx bx-cog' ></i>
-          <span class="links_name">Settings</span>
-        </a>
-        <span class="tooltip">Settings</span>
-      </li> -->
-
-      <li class="profile">
-          <div class="profile-details">
-              <div class="name_job" style="color:white;">GiveMeMusic &#8482;</div>
-          </div>
-              <form action="logOut.php" method="POST">
-                  <button class='bx bx-log-out' id="log_out" style="color: white;"></button>
-              </form>
-        </li>
-      </ul>
-    </div>
-    <section class="home-section">
-        <center>
-            <form>
-            <div id="logDiv" class="container-login100" style="display:block"><!-- Login  -->
-                <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
-                    <form action="/GiveMeMusic/API/user.php" class="login100-form validate-form">
-                        <span class="login100-form-title p-b-37">Sign into <br>Give Me Music!</span>
-
-                        <div class="wrap-input100 validate-input m-b-20" data-validate="Enter your email">
-                            <input class="input100" type="email" id="lMail" name="lMail" placeholder="Email">
-                            <span class="focus-input100"></span>
-                        </div>
-                        <div class="wrap-input100 validate-input m-b-25" data-validate = "Enter password">
-                            <input class="input100" type="password" id="lPsw" name="lPsw" placeholder="Password">
-                            <span class="focus-input100"></span>
-                        </div>
-
-                        <div class="container-login100-form-btn">
-                            <button class="login100-form-btn" onclick="userLogin();return false;">
-                            <!-- <button type="submit" -->
-                            <?php
-
-                            ?>
-                            <a>Sign In</a>
-                            </button>
-                        </div>
-
-                        <div class="text-center">
-                                <a class="txt2 hov1" id="logToReg">  
-                                <!-- href="register.php" -->
-                                    <br>
-                                    Sign Up!
-                                    <br>
-                                </a>    
-                                <script type="text/javascript">
-                                    document.getElementById("logToReg").onclick = function(){
-                                        document.getElementById("logDiv").style.display = 'none';
-                                        document.getElementById("regDiv").style.display = 'block';
-                                    }
-                                </script>  
-                        </div>
-                        <div class="text-center">
-                                <a id="logToForgot" class="txt2 hov1"><br>
-                                <!-- href="newPsw.php"  -->
-                                    Forgot password?
-                                </a>    
-                                <script type="text/javascript">
-                                    document.getElementById("logToForgot").onclick = function(){
-                                        document.getElementById("logDiv").style.display = 'none';
-                                        document.getElementById("forgotDiv").style.display = 'block';
-                                    }
-                                </script>  
-                        </div>
-                    </form>
-                        <div class="text-center p-t-57 p-b-20">
-                            <span class="txt1">Or login with</span>
-                            </div>
-                                <center>
-                                    <button>
-                                    <?php
-                                        if (isset($_GET['code'])) {
-                                            $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-                                            $client->setAccessToken($token['access_token']);
-                                            $google_oauth = new Google_Service_Oauth2($client);
-                                            $google_account_info = $google_oauth->userinfo->get();
-                                            $_SESSION['usr'] = $google_account_info->name;
-                                            $_SESSION['mail'] = $google_account_info->email;
-                                        }else{
-                                            echo "<a class='login100-social-item' href='".$client->createAuthUrl()."' >G</a>";
-                                        }
-                                    ?>
-                                    </button>
-                                </center>
-                            </div>
-                        </form>
-                </div>
-            </center>
-		</div>
-        </form>
-<center>
-<form method="POST">
-        <div id="regDiv" class="container-login100" style="display:none"><!--Registrazione -->
-			<div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
-				<form class="login100-form validate-form" method="PUT">
-					<span class="login100-form-title p-b-37">Sign up to <br>Give Me Music!</span>
-
-					<div class="wrap-input100 validate-input m-b-20" data-validate="Enter your username">
-						<input class="input100" type="text" id="rUsrn" name="rUsrn" placeholder="Username">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input m-b-20" data-validate="Enter your email">
-						<input class="input100" type="text" id="rMail" name="rMail" placeholder="Email">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input m-b-25" data-validate = "Enter password">
-						<input class="input100" type="password" id="rPsw" name="rPsw" placeholder="Password">
-						<span class="focus-input100"></span>
-					</div>
-					
-					<div class="container-login100-form-btn">
-						<button type="button" id="reg" class="login100-form-btn" onclick="userRegister()">
-                            <a>Sign Up</a>
-                        </button>
-					</div>
-                    
-					<div class="text-center">
-						<a id="regToLog" class="txt2 hov1">
-                        <!-- href="index.php" -->
-							<br>Already have an account? Sign In!<br>
-						</a>
-                        <script type="text/javascript">
-                                    document.getElementById("regToLog").onclick = function(){
-                                        document.getElementById("regDiv").style.display = 'none';
-                                        document.getElementById("logDiv").style.display = 'block';
-                                    }
-                        </script>  
-					</div>
-				</form>
-			</div>
-		</div>
-        </form>
-    </center>
-
-    <center>
-    <form method="POST">
-        <div id="forgotDiv" class="container-login100" style="display: none;"> <!-- Reset Password -->
-                <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
-                    <form class="login100-form validate-form" method="PATCH">
-                        <span class="login100-form-title p-b-37">Reset your password</span>
-
-                        <div class="wrap-input100 validate-input m-b-20" data-validate="Enter your email">
-                            <input class="input100" type="text" id="rMail" name="rMail" placeholder="Email">
-                            <span class="focus-input100"></span>
-                        </div>
-
-                        <div class="wrap-input100 validate-input m-b-25" data-validate = "Enter new password">
-                            <input class="input100" type="password" id="nPsw" name="rPsw" placeholder="Enter new password">
-                            <span class="focus-input100"></span>
-                        </div>
-                        
-                        <div class="wrap-input100 validate-input m-b-25" data-validate = "confirm password">
-                            <input class="input100" type="password" id="cnPsw" name="rPsw" placeholder="Confirm password">
-                            <span class="focus-input100"></span>
-                        </div>
-
-                        <div class="container-login100-form-btn">
-                            <button id="reg" class="login100-form-btn" onclick="resetPsw();return false;">
-                                <a>Reset</a>
-                            </button>
-                        </div>
-                        
-                        <div class="text-center">
-                            <a id="forToLog" class="txt2 hov1">
-                                <br>Sign In!<br>
-                                <script type="text/javascript">
-                                    document.getElementById("forToLog").onclick = function(){
-                                        document.getElementById("forToLog").style.display = 'none';
-                                        document.getElementById("logDiv").style.display = 'block';
-                                    }
-                        </script>  
-                            </a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            </form>
-    </center>
-
-    </section>
-    <script src="frontEnd/assets/jsFolder/script.js"></script>
-    </body>
-</html>
-<?php
-}
+<?php
+    session_start();
+    require_once "../config.php";
+    
+    $id_user = $_SESSION["user_id"];
+
+    if($_SERVER["REQUEST_METHOD"] == "POST" && !isset($_POST["del"])){//Inserimento like
+        header("Access-Control-Allow-Methods: POST");
+            $id_user = $_POST["id_user"];
+            $id_genre = $_POST["id_genre"];
+            $conn -> begin_transaction();
+            try{
+            $sql = "INSERT INTO likes (id_usr, id_genre) VALUES (?,?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('ii', $id_user, $id_genre);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $conn -> commit();
+            http_response_code(200);
+            response("",200, "Like Added");
+            return;
+            }catch(mysqli_sql_exception $exception){
+            	$mysqli -> rollback();
+                throw $exception;
+            }
+    }
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["del"])){//Eliminazione like
+        header("Access-Control-Allow-Methods: POST");
+            $id_user = $_POST["id_user"];
+            $id_genre = $_POST["id_genre"];
+            $sql = "DELETE FROM likes WHERE id_usr = ? AND id_genre = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('ii', $id_user, $id_genre);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            http_response_code(200);
+            response("",200, "Like Deleted");
+            return;
+    }
+    if($_SERVER["REQUEST_METHOD"] == "GET"){//Ottenimento likes 
+        header("Access-Control-Allow-Methods: GET");
+            $id_user = $_GET["id_user"];
+            $sql = "SELECT id, id_genre FROM likes WHERE id_usr = ".$id_user ;;
+            $result = $conn->query($sql);
+            if (mysqli_num_rows($result)>0){
+                $ret = "{";
+                while($row = $result->fetch_assoc()) {
+                    $ret = $ret."\"" . $row["id_genre"] . "\" :  " . $row["id"] . ",";
+                }
+            }else{
+                http_response_code(200);
+                responseGenre(200, "null");
+                return;
+            }
+            $id_like = $row["id"];
+            $id_genre = $row["id_genre"];
+            $ret = substr($ret, 0, -1) . "}";
+            http_response_code(200);
+            responseGenre(200, $ret);
+            return;
+    }
+
+    function response($message, $status, $response, $data=""){
+        echo json_encode(array("message" => $message, "status" => $status, "response" => $response,"data" => $data));
+        exit();
+    }
+
+    function responseGenre($code, $data){
+        echo " { \"code\":$code, \"data\":$data } ";
+        exit();
+    }
 ?>
